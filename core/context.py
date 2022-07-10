1d2af066ad99262d483893f1a88557189c4fbd4c
unknown <aguikenna@fiberswift.com>
2022-07-10 14:14:02 +0100

    ''

--- core/context.py
+++ core/context.py
@@ -11,17 +11,12 @@ def core(request) :
     ctx['site_name_verbose'] = "GTG Cargoose"
     ctx['site_name'] = "GTG Cargoose"
     ctx['site_name_full'] = "GTG Cargoose"
-    ctx['support_email'] = "support@afflus-trade.com"
-    ctx['site_email'] = "support@afflus-trade.com"
+    ctx['support_email'] = "support@gtgcargoose.com"
+    ctx['site_email'] = "support@gtgcargoose.com"
     ctx['site_phone'] = "+3594858"
     ctx['site_whatsapp_no'] = "+66658656fg6"
     ctx['site_address'] = "No 23 winston road new york"
-    ctx['ltc_wallet_address'] = "Ld7quXs9UXyRqQnxFSqwTqkoiWMCotUGdK"
-    ctx['usdt_bep20_wallet_address'] = "0x1aeeffb9bebfa454682db27ba57e3e6079c401b8"
-    ctx['usdt_trc20_wallet_address'] =  "TR1BVAZHnW8PcEdjhXqx6Bc6AF3aTW7i1X"
-    ctx['eth_wallet_address'] = "0x1aeeffb9bebfa454682db27ba57e3e6079c401b8"
-    ctx['btc_wallet_address'] = "bc1qezcxsgzq8g7sjtzt8vpz90tzpdnwnyqvmsgk68"
-    ctx['bnb_wallet_address'] = "0x1aeeffb9bebfa454682db27ba57e3e6079c401b8"
+
     
     return ctx  
 
