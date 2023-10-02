846782c314bec41b113154338a9cb71ce4406bbd
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2023-10-02 21:50:04 +0100

    ''

--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ class Country(models.Model) :
     short_name = models.CharField(max_length=5)
 
     def __str__(self) :
-        return "{}({})".format(self.name,self.short_name)
+        return "{}({})".format(self.name)
     
 
 
