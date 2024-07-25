d3bf51a6a7ead99b963e2bf1d2c9fb9b47f6e5d1
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2024-07-25 13:51:35 +0100

    ''

--- logistics/signals.py
+++ logistics/signals.py
@@ -8,29 +8,32 @@ from core.communication import LogisticsMail
 @receiver(post_save, sender= Shipment)
 def shipment_on_create(sender, instance, created, **kwargs):
     if  created :
+        pass
         #send email that the shipment has been registered
         #create first status 
-        StatusLog.objects.create(shipment = instance,status = "registered")
+        """StatusLog.objects.create(shipment = instance,status = "registered")
         mail = LogisticsMail(instance)
-        mail.send_shipment_created_mail()
+        mail.send_shipment_created_mail()"""
         
            
   
 @receiver(post_save, sender= TransitLog)
 def shipment_on_transit_update(sender, instance, created, **kwargs):
     if  created :
-        #send email that the shipment has been updated
+        pass
+        """#send email that the shipment has been updated
         mail = LogisticsMail(instance.shipment)
-        mail.send_shipment_progress_mail()
+        mail.send_shipment_progress_mail()"""
         
                
     
 @receiver(post_save, sender= StatusLog)
 def shipment_on_status_update(sender, instance, created, **kwargs):
     if  created :
-        #send email that the shipment has been updated
+        pass
+        """#send email that the shipment has been updated
         mail = LogisticsMail(instance.shipment)
-        mail.send_shipment_progress_mail()
+        mail.send_shipment_progress_mail()"""
         
             
 
