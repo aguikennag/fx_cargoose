127937b0076bcdd4bf17485d201534efeba56d16
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2024-08-12 18:13:07 +0100

    ''

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python manage.py loaddata dump.json
+#python manage.py loaddata dump.json
 
 
 
